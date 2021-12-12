fa072a442ea77c2aafb9e7cbb75399b097f367f7
GirijalaAditya <girijalaaditya@gmail.com>
2021-12-12 12:09:16 +0530

    added code

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-def call()
+def call(String imageName)
 {
     echo "Building the Docker Image"
     sh "docker build -t gradle-app ."
-    sh "docker tag gradle-app adityadevops/gradle-app:${env.BUILD_ID}"
+    sh "docker tag gradle-app $imageName:${env.BUILD_ID}"
 }
